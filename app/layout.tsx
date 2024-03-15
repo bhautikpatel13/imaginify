4369a2844ebd5d390fed696e5d30a01befdcbed2
bhautikpatel13 <bhautikpatel464@gmail.com>
2024-03-16 00:19:44 +0530

    31th commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{ variables: { colorPrimary: '#624cf5' }}}>
+    <ClerkProvider appearance={{ variables: { colorPrimary: '#fff' }}}>
       <html lang="en">
         <body className={ cn("font-IBMPlex antialiased", IBMPlex.variable ) }>{children}</body>
       </html>
